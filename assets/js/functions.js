5d180b70ebbdbd1ae6591c1007877b71deaa0512
sanakkgec <64202442+sanakkgec@users.noreply.github.com>
2020-12-21 19:55:20 +0530

    Added a move up button in footer

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,3 +7,12 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
 }
+
+$(document).ready(function () {
+    // click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+});
